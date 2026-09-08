772fa3c72734363df5d7cfbb5a3f8c21e93377ac
Nandani Kumari <nraj21.nandani@gmail.com>
2026-09-08 23:09:53 +0530

    Add pattern10 method for star triangle pattern
    Implement a star triangle pattern printing method.

---
+++ 02-Patterns/Pattern-10_star_triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void pattern10(int n) {
+            for(int i = 0; i < n; i++) {
+              for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+           for(int i = n-1; i >= 1; i--) {
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }       
+}
+    
